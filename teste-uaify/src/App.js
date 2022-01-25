16362b737040f9294078e2c1ef6ada9e8d6ebe63
DESKTOP-DM6LOBN\Acer <Vincius Fredeanelle>
2022-01-25 18:39:08 -0300

    Teste finalizado

--- teste-uaify/src/App.js
+++ teste-uaify/src/App.js
@@ -1,3 +1,6 @@
+import React, {useState, useEffect } from 'react';
+import './index.css';
+import useInput from "./Hooks/useInput"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Row, Col, Form, FormGroup, Input, Label, Button} from "reactstrap";
 import Banner1 from "./Images/Banner1.png";
@@ -6,15 +9,44 @@ import Grupo264 from "./Images/Grupo264.png";
 import instagram from "./Images/instagram.png";
 import facebook from "./Images/facebook.png";
 import youTube from "./Images/youTube.png";
+import logo from "./Images/logo.png";
 
 function App() {
 
+  const {form, onChange, resetaEntrada} = useInput({
+    nome: '',
+    email:'',
+    telefone: '',
+    produto: '',
+    estado: '',
+    cidade: '',
+    mensagem: '',
+  })
+
+  const [politica, setPolitica] = useState(false)
+
+  const handleInputChange = event => {
+    const { name, value} = event.target;
+    
+    onChange(name, value);
+  };
+
   const enviar = ()=>{
-    alert("Formulário enviado com sucesso!")
+    console.log(form)
+    if(form.nome && form.email && form.telefone && form.produto && form.cidade !== null && politica === true){
+      alert("Formulário enviado com sucesso!")
+    }else{
+      alert("Preencha todos os campos obrigatorios!")
+    }
+  }
+
+  const aceitaPolitica = ()=>{
+    setPolitica(!politica)
   }
 
   return (
-    <Container  style={{maxWidth: "100%", padding: "0"}}>
+    <Container className={"Fonte"}  style={{maxWidth: "100%", padding: "0"}}>
+      <img  className={"Logo"} src={logo} style={{position: 'absolute', zIndex: 0, marginLeft: "80%",}}/>
       <Row>
         <Col>
           <img  src={Banner1}/>
@@ -56,10 +88,10 @@ function App() {
       </Row>
 
       <Row style={{paddingTop: "5%", borderBottomStyle: "dotted", paddingBottom: "5%", borderBottomWidth: "3px", borderColor: "grey"}}>
-        <Col xs="4" style={{paddingLeft: "5%"}}>
+        <Col xs="4" style={{paddingLeft: "15%"}}>
           <img src={Grupo267}/>
         </Col>
-        <Col>
+        <Col style={{paddingLeft: "8%"}}>
           <h2 style={{color: "#FDD000"}}><b>MONITORES</b></h2>
           <p>Tenha o <b>gerenciamento preciso</b> do campo <b>nas suas mãos</b> com tela colorida e recursos de alta precisão. Dessa forma, você aproveita muito mais os insumos e</p>
         </Col>
@@ -67,14 +99,14 @@ function App() {
         </Col>
       </Row>
 
-      <Row style={{paddingTop: "5%", }}>
+      <Row style={{paddingTop: "5%"}}>
         <Col xs="4">
         </Col>
         <Col style={{textAlign: "right"}}>
           <h2 style={{color: "#FDD000",}}><b>GESTÃO DE FROTA</b></h2>
           <p><b>Através de rede móvel,</b> você pode se <b>conectar com as suas máquinas New Holland de onde estiver.</b> E ainda, pode enviar e receber informações <b>em tempo real.</b></p>
         </Col>
-        <Col xs="4" style={{paddingLeft: "5%"}}>
+        <Col xs="4" style={{paddingRight: "15%"}}>
           <img src={Grupo264}/>
         </Col>
       </Row>
@@ -84,101 +116,131 @@ function App() {
         </Col>
         <Col xs="6">
           <h2 style={{textAlign: "center"}}>Quer conhecer a solução ideal para o seu campo? <br/> Fale agora mesmo com nosso time de especialistas.</h2>
-          <Form style={{paddingTop: "5%"}}>
-            <FormGroup>
+          <form style={{paddingTop: "5%"}}>
+            <div>
               <Label for="Nome">
                 Nome
               </Label>
               <Input
                 id="Nome"
-                name="Nome"
+                name="nome"
+                value={form.nome}
+                onChange={handleInputChange}
                 placeholder=""
                 type="text"
+                required
               />
-            </FormGroup>
-            <FormGroup>
+            </div>
+            <div>
               <Label for="Telefone">
                 Telefone
               </Label>
               <Input
                 id="Telefone"
-                name="Telefone"
+                name="telefone"
+                value={form.telefone}
+                onChange={handleInputChange}
                 placeholder=""
                 type="number"
+                required
               />
-            </FormGroup>
-            <FormGroup>
+            </div>
+            <div>
               <Label for="Email">
                 Email
               </Label>
               <Input
                 id="Email"
-                name="Email"
+                name="email"
+                value={form.email}
+                onChange={handleInputChange}
                 placeholder=""
                 type="email"
+                required
               />
-            </FormGroup>
-            <FormGroup>
+            </div>
+            <div>
               <Label for="Estado">
                 Estado
               </Label>
               <Input
                 id="Estado"
-                name="Estado"
+                name="estado"
+                value={form.estado}
+                onChange={handleInputChange}
                 type="select"
+                required
               >
                 <option>
                   
                 </option>
-                <option>
+                <option 
+                  value={"Minas Gerais"}
+                >
                   Minas Gerais
                 </option>
-                <option>
+                <option
+                  value={"São Paulo"}
+                >
                   São Paulo
                 </option>
               </Input>
-            </FormGroup>
-            <FormGroup>
+            </div>
+            <div>
               <Label for="Cidade">
                 Cidade
               </Label>
               <Input
                 id="Cidade"
-                name="Cidade"
+                name="cidade"
                 type="select"
+                value={form.cidade}
+                onChange={handleInputChange}
+                required
               >
                 <option>
                   
                 </option>
-                <option>
+                <option
+                  value={"Belo Horizonte"}
+                >
                   Belo Horizonte
                 </option>
-                <option>
+                <option
+                  value={"São Paulo"}
+                >
                   São Paulo
                 </option>
               </Input>
-            </FormGroup>
-            <FormGroup>
+            </div>
+            <div>
               <Label for="Produto">
                 Selecione um Produto
               </Label>
               <Input
                 id="Produto"
-                name="Produto"
+                name="produto"
                 type="select"
+                value={form.produto}
+                onChange={handleInputChange}
+                required
               >
                 <option>
                   
                 </option>
-                <option>
+                <option
+                  value={"Produto 1"}
+                >
                   Produto 1
                 </option>
-                <option>
+                <option
+                  value={"Produto 2"}
+                >
                   Produto 2
                 </option>
               </Input>
-            </FormGroup>
-            <FormGroup>
+            </div>
+            <div>
               <Label for="Mensagem">
                 Mensagem
               </Label>
@@ -189,31 +251,32 @@ function App() {
                 placeholder=""
                 type="textarea"
               />
-            </FormGroup>
-            <FormGroup check>
+            </div>
+            <div check>
               <Input type="checkbox" />
               {' '}
               <Label check>
                 Desejo receber mais informações sobre produtos e serviços da New Holland
               </Label>
-            </FormGroup>
-            <FormGroup check>
-              <Input type="checkbox" />
+            </div>
+            <div check>
+              <Input type="checkbox" value={politica} onClick={aceitaPolitica} required/>
               {' '}
               <Label check>
                 Li e concordo com a <b>Política de Privacidade</b> deste site
               </Label>
-            </FormGroup>
-            <FormGroup check style={{paddingTop: "3%"}}>
-              <Button onClick={enviar} style={{width: "90%", backgroundColor: "#003E85", padding: "2%"}}>
+            </div>
+            <div check style={{paddingTop: "3%"}}>
+              <button onClick={enviar} style={{width: "100%", backgroundColor: "#003E85", padding: "2%"}}>
                 Submit
-              </Button>
-            </FormGroup>
-          </Form>
+              </button>
+            </div>
+          </form>
         </Col>
         <Col>
         </Col>
       </Row>
+      
       <Row style={{marginTop: "10%", minHeight: "30vh", backgroundColor: "#003E85", display: "flex", justifyContent: "center", alignItems: "center"}}>
         <Col style={{textAlign: "center"}}>
           <span style={{color: "white"}}>Acesse <b>NEWHOLLAND.COM.BR</b></span>
